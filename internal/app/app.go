aaac653264b0aa5673f49bb87df36bfbb660b72f
Романов Максим Петрович <mpromanov@corp.finam.ru>
2024-03-16 01:08:49 +0300

    [T1] Base structure

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,30 +1,80 @@
 package app
 
 import (
-	"temp/internal/app/models"
+	"os"
+	m "temp/internal/app/models"
+	"temp/internal/app/router"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
+	"github.com/gofiber/fiber/v2/log"
+	"gopkg.in/yaml.v3"
 )
 
-type App struct{}
+type AppFlags struct {
+	ConfigPath string
+}
+
+type App struct {
+	cfg *m.Config
+}
 
 func New() App {
 	return App{}
 }
 
-func (a *App) Run() {
-	app := fiber.New()
+func (a *App) Init(f *AppFlags) {
+	log.Info("loading app config")
+	if err := a.LoadConfigFromFile(f.ConfigPath); err != nil {
+		log.Fatalf("unable to load app config %s", err)
+	}
+}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		r := Response{
-			Result: models.Tmp{
-				ID:      uuid.New().String(),
-				Message: "Faggot",
-			},
-		}
-		return c.JSON(r)
+func (a *App) Run(f *AppFlags) {
+	a.Init(f)
+
+	app := fiber.New(fiber.Config{
+		ReadTimeout:  a.cfg.Server.ReadTimeout,
+		WriteTimeout: a.cfg.Server.WriteTimeout,
+		IdleTimeout:  a.cfg.Server.IdleTimeout,
 	})
 
-	app.Listen(":8000")
+	r := router.NewRouter(app, a.cfg)
+	r.Setup()
+
+	app.Listen(a.cfg.Server.Addr)
+}
+
+func (a *App) LoadConfigFromFile(path string) error {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	var cfg m.RawConfig
+
+	err = yaml.Unmarshal(file, &cfg)
+	if err != nil {
+		return err
+	}
+
+	a.cfg = &m.Config{
+		Version: cfg.Version,
+		Env:     cfg.Env,
+		Database: m.DatabaseConfig{
+			Conn: cfg.Database.Conn,
+		},
+		Server: m.ServerConfig{
+			Addr:         cfg.Server.Addr,
+			ReadTimeout:  time.Duration(cfg.Server.ReadTimeout) * time.Second,
+			WriteTimeout: time.Duration(cfg.Server.WriteTimeout) * time.Second,
+			IdleTimeout:  time.Duration(cfg.Server.IdleTimeout) * time.Second,
+		},
+		Auth: m.AuthConfig{
+			AccessTokenTTL:  time.Duration(cfg.Auth.AccessTokenTTL) * time.Minute,
+			RefreshTokenTTL: time.Duration(cfg.Auth.RefreshTokenTTL) * time.Minute,
+		},
+	}
+
+	return err
 }
